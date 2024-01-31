1331890b0187135d50e2c807ef51ce2fe363ab43
yuH111 <qhuynguyenthai@gmail.com>
2024-02-01 00:07:42 +0700

    WorkCPP

--- src/test.cpp
+++ src/test.cpp
@@ -9,6 +9,8 @@ using namespace std;
                 cin >> b;
             cout <<"Nhap c";
                 cin >> c;
+            cout <<"Nhap d: ";
+                cin >> d;
             cout <<"Tong cua a b c la: " << a+b+c;
             cout <<"Dien tich Hinh Chu Nhat la: " << a * b <<".";
             cout <<"\nChu vi Hinh Chu Nhat la: " << ( a+b) *2 <<".";
